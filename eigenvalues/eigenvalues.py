9a1ee428dd075c0797d98f1afb114c313f33149c
CedricJ7 <122177053+CedricJ7@users.noreply.github.com>
2026-08-17 18:50:23 +0200

    Solve eigenvalues

---
+++ eigenvalues/eigenvalues.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+def calculate_eigenvalues(matrix):
+    """
+    Calculate eigenvalues of a square matrix.
+    """
+    try: 
+        matrix  = np.asarray(matrix)
+    except: 
+        return None
+    if matrix.ndim != 2 or matrix.shape[0] != matrix.shape[1]:
+        return None
+    return(np.linalg.eigvals(matrix))
